c88ce8a14dba9092f85d87fbcd91b812f5d29174
ankur2710 <ankurghosh271005@gmail.com>
2026-04-03 19:54:32 +0530

    Implement number guessing game logic

--- Guess_game.py
+++ Guess_game.py
@@ -1,1 +1,15 @@
+import random
+random.randint(1, 100)
+jackpot = random.randint(1, 100)
+guess= int(input("Chal guess kar "))
+counter = 1
 
+while guess != jackpot:
+    if guess < jackpot:
+        print("guess higher")
+    else:
+        print("guess lower")
+    guess = int(input("Guess The Number Between 1-100  "))
+    counter += 1
+print("Correct Answer ")
+print("you took", counter, "attempts")
