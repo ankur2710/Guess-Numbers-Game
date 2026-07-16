7fd23ec2fa26ba2d80669829c343f08519ce1735
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-16 23:30:01 +0530

    Fix indentation and improve guessing prompts

--- Guess_game.py
+++ Guess_game.py
@@ -6,4 +6,8 @@ counter = 1
 while guess != jackpot:
     if guess < jackpot:
         print("guess higher")
-  
+    else:
+        print("guess lower")
+    guess = int(input("Guess The Number Between 1-100  "))
+    counter += 1
+
